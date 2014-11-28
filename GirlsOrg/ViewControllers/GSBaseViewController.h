1ee085d9e53f9a4f6d2908f204bc5d6ced1225ef
lxy <lglucifer@126.com>
2014-11-29 00:10:36 +0800

    -M 'appdelegate'

---
+++ GirlsOrg/ViewControllers/GSBaseViewController.h
@@ -0,0 +1,14 @@
+//
+//  GSBaseViewController.h
+//  GirlsOrg
+//
+//  Created by LiuXiaoyu on 11/28/14.
+//  Copyright (c) 2014 uzero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UIViewController+GSTarBarController.h"
+
+@interface GSBaseViewController : UIViewController
+
+@end
